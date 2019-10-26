fcc360a480581875d519d44c8392ae3fdae83c3b
Nadia-Kadri <nadiagrace96@gmail.com>
2019-10-25 22:01:39 -0400

    ajax call and append to page

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 let initialButtons = ["happy", "sad", "hungry", "hyper", "tired", "determined", "jealous", "strong"];
 
+
 for (let i = 0; i < initialButtons.length; i++) {
     $("<button>").attr("class", "gif-button")
+                 .attr("data-gif", initialButtons[i])
                  .text(initialButtons[i])
                  .appendTo("#gif-buttons")
-}
+};
+
 
 $("#add-button").on("click", function(event) {
     event.preventDefault();
     addButton()
-})
+});
+
 
 function addButton() {
     let userGif = $("#search-term").val().trim()
@@ -19,11 +23,46 @@ function addButton() {
     }
 
     $("<button>").attr("class", "gif-button")
+                 .attr("data-gif", userGif)
                  .text(userGif)
                  .appendTo("#gif-buttons")
-}
+};
+
+
+$(document).on("click", ".gif-button", function() {
+    $("#display-gif").empty()
+
+    let gif = $(this).attr("data-gif");
+
+    const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=QFgJUbUNCHg590GuY69pniXKIERiCZUN&limit=10"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+      console.log(response.data);
+    
+      for (let i = 0; i < response.data.length; i++) {
+      let animate = response.data[i].images.downsized.url
+      let still = response.data[i].images.downsized_still.url
+      console.log(animate)
+      console.log(still)
+
+      let newGif = $("<img>")
+                  .addClass("mood-gif")
+                  .attr("src", animate)
+                  .attr("data-state", "still")
+                  .attr("data-animate", animate)                            
+                  .attr("data-still", still)      
+                  
+      newGif.appendTo("#display-gif")
+      }
+    });
+});
 
 
 
-// QFgJUbUNCHg590GuY69pniXKIERiCZUN
+
 
